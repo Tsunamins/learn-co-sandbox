790c12867b8313e0d0909e851f5329b4aec988b4
Alison Reilly <reillyamr@gmail.com>
2019-01-16 20:41:41 +0000

    first commit

---
+++ hotel-FandB-jobs/lib/hotel/FandB/jobs.rb
@@ -0,0 +1,10 @@
+require "hotel/FandB/jobs/version"
+
+module Hotel
+  module FandB
+    module Jobs
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ hotel-FandB-jobs/lib/hotel/FandB/jobs/version.rb
@@ -0,0 +1,7 @@
+module Hotel
+  module FandB
+    module Jobs
+      VERSION = "0.1.0"
+    end
+  end
+end
