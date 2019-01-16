925309a34b1158caba850ee767d2f2d8cc6fb1c0
Alison Reilly <reillyamr@gmail.com>
2019-01-16 20:53:04 +0000

    first commit

---
+++ hotel-jobs/lib/hotel/jobs.rb
@@ -0,0 +1,8 @@
+require "hotel/jobs/version"
+
+module Hotel
+  module Jobs
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ hotel-jobs/lib/hotel/jobs/version.rb
@@ -0,0 +1,5 @@
+module Hotel
+  module Jobs
+    VERSION = "0.1.0"
+  end
+end
